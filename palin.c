4d41a0a2a14a69e2060265b51520a26d179e21c2
Jesse McCarville <pango3001@gmail.com>
2020-10-01 14:14:43 -0500

    update palin

--- palin.c
+++ palin.c
@@ -17,24 +17,44 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
-void isPalindrome(char str[]);
+bool isPalindrome(char str[]);
+
+
+typedef struct{
+	int id;
+        int key_t;  //key_t key;
+        char strings[64][64]; // array of strings
+} shared_memory;
+
+
 
 int main(int argc, char ** argv){
-	
+
+	bool palin = isPalindrome(argv[1]);
+		
+
+
+	if(palin){
+		printf("%s is palin\n", argv[1]);
+	}
+	else{
+		printf("%s is not\n", argv[1]);
+	}
 	return 0;
 }
 
 
-void isPalindrome(char str[]) {
+bool isPalindrome(char str[]) {
+	bool palin = true;
 	int l = 0;
 	int h = strlen(str) - 1;
 	while (h > l){
-        	if(str[l++] != str[h--]){
-			printf("%s is Not Palindrome", str); 
-			return;
+        	if(str[l++] != str[h--]){		 
+			return false;
 		}
 	}
-	printf("%s is palindrome", str); 
+	return true; 
 }
 
